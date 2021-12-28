9fd61f98e93808e46a0664664584a579af419b9d
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 21:45:41 +0700

    chore: quote no bg component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -25,18 +25,7 @@
                         <x-blog.without-image />
                         <x-blog.gallery />
                         <x-blog.quote />
-                        <div class="blog-post quote-post no-bg">
-                            <div class="blog-info">
-                                <span class="category">English, Teachers, Shelly</span>
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3><a href="post.html" title="">Link Post Format</a></h3>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->								
+                        <x-blog.quote-no-bg />
                         <div class="blog-post video-post">
                             <div class="blog-thumbnail">
                                 <a href="#" title="">
---
+++ resources/views/components/blog/quote-no-bg.blade.php
@@ -0,0 +1,37 @@
+<div class="blog-post quote-post no-bg">
+    <div class="blog-info">
+        <span class="category">
+            English, Teachers, Shelly
+        </span>
+        <ul class="meta">
+            <li>
+                <a href="#" title="">
+                    17/09/2020
+                </a>
+            </li>
+            <li>
+                <a href="#" title="">
+                    by Admin
+                </a>
+            </li>
+            <li>
+                <img src="assets/img/icon13.png" alt="" />
+                <a href="#" title="">
+                    Teachers,
+                </a>
+                <a href="#" title="">
+                    School
+                </a>
+            </li>
+        </ul>
+        <h3>
+            <a href="post.html" title="">
+                Link Post Format
+            </a>
+        </h3>
+        <a href="post.html" title="" class="read-more">
+            Read
+            <i class="fa fa-long-arrow-alt-right"></i>
+        </a>
+    </div>
+</div>
