c685d711fd989a06135d2cde3d08e293d4294de5
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 22:47:11 +0700

    chore: latest posts comment

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -57,39 +57,7 @@
                     </div>
 
                     <x-blog.categories />
-
-                    <div class="widget widget-posts">
-                        <h3 class="widget-title">Latest Posts</h3>
-                        <div class="wd-posts">
-                            <div class="wd-post d-flex flex-wrap">
-                                <div class="wd-thumb">
-                                    <img src="https://via.placeholder.com/52x52" alt="">
-                                </div>
-                                <div class="wd-info">
-                                    <h3><a href="post.html" title="">Duis tempor eros tortor, a ornare</a></h3>
-                                    <span>17/09/2020</span>
-                                </div>
-                            </div><!--wd-post end-->
-                            <div class="wd-post d-flex flex-wrap">
-                                <div class="wd-thumb">
-                                    <img src="https://via.placeholder.com/52x52" alt="">
-                                </div>
-                                <div class="wd-info">
-                                    <h3><a href="post.html" title="">Duis tempor eros tortor, a ornare</a></h3>
-                                    <span>17/09/2020</span>
-                                </div>
-                            </div><!--wd-post end-->
-                            <div class="wd-post d-flex flex-wrap">
-                                <div class="wd-thumb">
-                                    <img src="https://via.placeholder.com/52x52" alt="">
-                                </div>
-                                <div class="wd-info">
-                                    <h3><a href="post.html" title="">Duis tempor eros tortor, a ornare</a></h3>
-                                    <span>17/09/2020</span>
-                                </div>
-                            </div><!--wd-post end-->
-                        </div><!--wd-posts end-->
-                    </div><!--widget-posts end-->
+                    <x-blog.latest-posts />
                     <div class="widget widget-comments">
                         <h3 class="widget-title">Recent Comments</h3>
                         <ul>
---
+++ resources/views/components/blog/latest-posts.blade.php
@@ -0,0 +1,50 @@
+<div class="widget widget-posts">
+    <h3 class="widget-title">Latest Posts</h3>
+    <div class="wd-posts">
+        <div class="wd-post d-flex flex-wrap">
+            <div class="wd-thumb">
+                <img src="https://via.placeholder.com/52x52" alt="">
+            </div>
+            <div class="wd-info">
+                <h3>
+                    <a href="post.html" title="">
+                        Duis tempor eros tortor, a ornare
+                    </a>
+                </h3>
+                <span>
+                    17/09/2020
+                </span>
+            </div>
+        </div>
+        <div class="wd-post d-flex flex-wrap">
+            <div class="wd-thumb">
+                <img src="https://via.placeholder.com/52x52" alt="">
+            </div>
+            <div class="wd-info">
+                <h3>
+                    <a href="post.html" title="">
+                        Duis tempor eros tortor, a ornare
+                    </a>
+                </h3>
+                <span>
+                    17/09/2020
+                </span>
+            </div>
+        </div>
+        <div class="wd-post d-flex flex-wrap">
+            <div class="wd-thumb">
+                <img src="https://via.placeholder.com/52x52" alt="">
+            </div>
+            <div class="wd-info">
+                <h3>
+                    <a href="post.html" title="">
+                        Duis tempor eros tortor, a ornare
+                    </a>
+                </h3>
+                <span>
+                    17/09/2020
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
