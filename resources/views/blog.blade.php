23d6b007c74cdc249d7e631fd9f361edbb3e42a5
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 21:40:15 +0700

    chore: quote component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -24,19 +24,7 @@
                         <x-blog.standard />
                         <x-blog.without-image />
                         <x-blog.gallery />
-                        <div class="blog-post quote-post">
-                            <div class="blog-info">
-                                <span class="category">English, Teachers, Shelly</span>
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <p>Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec vehicula eget quam maximus interdum. Duis ultrices sapien</p>
-                                <h4>Polina Podolski</h4>
-                                <span>Mother</span>
-                            </div>
-                        </div><!--blog-post end-->
+                        <x-blog.quote />
                         <div class="blog-post quote-post no-bg">
                             <div class="blog-info">
                                 <span class="category">English, Teachers, Shelly</span>
