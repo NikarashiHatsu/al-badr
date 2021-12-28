482f28fb4b91af6e0115df5d768e4d55e8446c6e
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 21:27:17 +0700

    chore: standard blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -20,57 +20,8 @@
             <div class="col-lg-8">
                 <div class="blog-section p-0 posts-page">
                     <div class="blog-posts">
-                        <div class="blog-post">
-                            <div class="blog-thumbnail">
-                                <a href="#" title="">
-                                    <img src="https://via.placeholder.com/2172x1448" alt="" class="w-100">
-                                </a>
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3 class="stick"><a href="post.html" title="">Standard Post Sticky</a></h3>
-                                <p>Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec vehicula eget quam maximus interdum. Duis ultrices sapien eget metus luctus finibus. Morbi nec lobortis ante, sed condimentum nisl. Nam laoreet ullamcorper magna et fringilla</p>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
-                        <div class="blog-post">
-                            <div class="blog-thumbnail">
-                                <a href="#" title="">
-                                    <img src="https://via.placeholder.com/1680x1120" alt="" class="w-100">
-                                </a>
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3><a href="post.html" title="">Standard Post Format</a></h3>
-                                <p>Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec vehicula eget quam maximus interdum. Duis ultrices sapien eget metus luctus finibus. Morbi nec lobortis ante, sed condimentum nisl. Nam laoreet ullamcorper magna et fringilla</p>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
-                        <div class="blog-post without-thumb">
-                            <div class="blog-thumbnail">
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3><a href="post.html" title="">Post Without Image</a></h3>
-                                <p>Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec vehicula eget quam maximus interdum. Duis ultrices sapien eget metus luctus finibus. Morbi nec lobortis ante, sed condimentum nisl. Nam laoreet ullamcorper magna et fringilla</p>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
+                        <x-blog.standard />
+                        <x-blog.standard />
                         <div class="blog-post gallery-post">
                             <div class="blog-carousel">
                                 <div class="blog-thumbnail">
