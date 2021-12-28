306579929a18c3ca24fc695907c51aab7345aa37
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 22:53:38 +0700

    chore: latest comments

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -58,14 +58,7 @@
 
                     <x-blog.categories />
                     <x-blog.latest-posts />
-                    <div class="widget widget-comments">
-                        <h3 class="widget-title">Recent Comments</h3>
-                        <ul>
-                            <li><a href="#" title="">Admin</a> in tempor eros tortor, a ornare </li>
-                            <li><a href="#" title="">Admin</a> in tempor eros tortor, a ornare </li>
-                            <li><a href="#" title="">Admin</a> in tempor eros tortor, a ornare </li>
-                        </ul>
-                    </div><!--widget-comments end-->
+                    <x-blog.latest-comments />
                     <div class="widget widget-archives">
                         <h3 class="widget-title">Archives</h3>
                         <ul>
---
+++ resources/views/components/blog/latest-comments.blade.php
@@ -0,0 +1,23 @@
+<div class="widget widget-comments">
+    <h3 class="widget-title">Recent Comments</h3>
+    <ul>
+        <li>
+            <a href="#" title="">
+                Admin
+            </a>
+            in tempor eros tortor, a ornare
+        </li>
+        <li>
+            <a href="#" title="">
+                Admin
+            </a>
+            in tempor eros tortor, a ornare
+        </li>
+        <li>
+            <a href="#" title="">
+                Admin
+            </a>
+            in tempor eros tortor, a ornare
+        </li>
+    </ul>
+</div>
