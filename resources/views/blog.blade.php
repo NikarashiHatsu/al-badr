5943485cc115c558ef51c3815f749a61e9fe0cdb
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 22:13:37 +0700

    chore: chat component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -28,58 +28,7 @@
                         <x-blog.quote-no-bg />
                         <x-blog.video />
                         <x-blog.title-less />
-                        <div class="blog-post">
-                            <div class="blog-thumbnail">
-                                <a href="#" title="">
-                                    <img src="https://via.placeholder.com/1680x1120" alt="" class="w-100">
-                                </a>
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3><a href="post.html" title="">Chat Post Format</a></h3>
-                                <ul class="chat-format">
-                                    <li>
-                                        <strong>Kate:</strong>
-                                        <p>Donec dignissim magna eget turpis sagittis porttitor. Sed eu sem ornare, placerat nunc sit amet, vehicula urna. Sed fringilla placerat hendrerit. Vivamus tempus, dolor nec rutrum sollicitudin, velit turpis finibus nibh, ac egestas arcu orci eu turpis</p>
-                                    </li>
-                                    <li>
-                                        <strong>Kristine:</strong>
-                                        <p>Phasellus pulvinar lacus dolor, ac posuere urna vehicula elementum</p>
-                                    </li>
-                                    <li>
-                                        <strong>Bob:</strong>
-                                        <p>Phasellus pulvinar lacus dolor, ac posuere urna vehicula elementum</p>
-                                    </li>
-                                    <li>
-                                        <strong>Sean:</strong>
-                                        <p>Vivamus lobortis mollis ex, sit amet porta neque sagittis ac. Donec tincidunt sem ac enim euismod, eu ullamcorper ex Vestibulum</p>
-                                    </li>
-                                </ul>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
-                        <div class="blog-post">
-                            <div class="blog-thumbnail">
-                                <a href="#" title="">
-                                    <img src="https://via.placeholder.com/1301x868" alt="" class="w-100">
-                                </a>
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <h3><a href="#" title="">Status Post Format. Phasellus aliquet felis et dolor lacinia commodo…</a></h3>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
+                        <x-blog.chat />
                     </div><!--blog-posts end-->
                 </div><!--blog-section end-->
 
---
+++ resources/views/components/blog/chat.blade.php
@@ -0,0 +1,88 @@
+<div class="blog-post">
+    <div class="blog-thumbnail">
+        <a href="#" title="">
+            <img
+                src="https://via.placeholder.com/1680x1120"
+                alt=""
+                class="w-100"
+            />
+        </a>
+        <span class="category">
+            English, Teachers, Shelly
+        </span>
+    </div>
+    <div class="blog-info">
+        <ul class="meta">
+            <li>
+                <a href="#" title="">
+                    17/09/2020
+                </a>
+            </li>
+            <li>
+                <a href="#" title="">
+                    by Admin
+                </a>
+            </li>
+            <li>
+                <img src="assets/img/icon13.png" alt="" />
+                <a href="#" title="">
+                    Teachers,
+                </a>
+                <a href="#" title="">
+                    School
+                </a>
+            </li>
+        </ul>
+        <h3>
+            <a href="post.html" title="">
+                Chat Post Format
+            </a>
+        </h3>
+        <ul class="chat-format">
+            <li>
+                <strong>
+                    Kate:
+                </strong>
+                <p>
+                    Donec dignissim magna eget turpis sagittis porttitor. Sed
+                    eu sem ornare, placerat nunc sit amet, vehicula urna. Sed
+                    fringilla placerat hendrerit. Vivamus tempus, dolor nec
+                    rutrum sollicitudin, velit turpis finibus nibh, ac egestas
+                    arcu orci eu turpis
+                </p>
+            </li>
+            <li>
+                <strong>
+                    Kristine:
+                </strong>
+                <p>
+                    Phasellus pulvinar lacus dolor, ac posuere urna vehicula
+                    elementum
+                </p>
+            </li>
+            <li>
+                <strong>
+                    Bob:
+                </strong>
+                <p>
+                    Phasellus pulvinar lacus dolor, ac posuere urna vehicula
+                    elementum
+                </p>
+            </li>
+            <li>
+                <strong>
+                    Sean:
+                </strong>
+                <p>
+                    Vivamus lobortis mollis ex, sit amet porta neque sagittis
+                    ac. Donec tincidunt sem ac enim euismod, eu ullamcorper ex
+                    Vestibulum
+                </p>
+            </li>
+        </ul>
+        <a href="post.html" title="" class="read-more">
+            Read
+            <i class="fa fa-long-arrow-alt-right"></i>
+        </a>
+    </div>
+</div>
