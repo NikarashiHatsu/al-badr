54e9ed51eb1dad66b78d953b239629ed75e1ddd6
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 22:03:38 +0700

    chore: title-less component

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -27,23 +27,7 @@
                         <x-blog.quote />
                         <x-blog.quote-no-bg />
                         <x-blog.video />
-                        <div class="blog-post without-title">
-                            <div class="blog-thumbnail">
-                                <a href="#" title="">
-                                    <img src="https://via.placeholder.com/1340x894" alt="" class="w-100">
-                                </a>
-                                <span class="category">English, Teachers, Shelly</span>
-                            </div>
-                            <div class="blog-info">
-                                <ul class="meta">
-                                    <li><a href="#" title="">17/09/2020</a></li>
-                                    <li><a href="#" title="">by Admin</a></li>
-                                    <li><img src="assets/img/icon13.png" alt="" /><a href="#" title="">Teachers,</a><a href="#" title=""> School</a></li>
-                                </ul>
-                                <p>Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec vehicula eget quam maximus interdum. Duis ultrices sapien eget metus luctus finibus. </p>
-                                <a href="post.html" title="" class="read-more">Read <i class="fa fa-long-arrow-alt-right"></i></a>
-                            </div>
-                        </div><!--blog-post end-->
+                        <x-blog.title-less />
                         <div class="blog-post">
                             <div class="blog-thumbnail">
                                 <a href="#" title="">
---
+++ resources/views/components/blog/title-less.blade.php
@@ -0,0 +1,47 @@
+<div class="blog-post without-title">
+    <div class="blog-thumbnail">
+        <a href="#" title="">
+            <img
+                src="https://via.placeholder.com/1340x894"
+                alt=""
+                class="w-100"
+            />
+        </a>
+        <span class="category">
+            English, Teachers, Shelly
+        </span>
+    </div>
+    <div class="blog-info">
+        <ul class="meta">
+            <li>
+                <a href="#" title="">
+                    17/09/2020
+                </a>
+            </li>
+            <li>
+                <a href="#" title="">
+                    by Admin
+                </a>
+            </li>
+            <li>
+                <img src="assets/img/icon13.png" alt="" />
+                <a href="#" title="">
+                    Teachers,
+                </a>
+                <a href="#" title="">
+                    School
+                </a>
+            </li>
+        </ul>
+        <p>
+            Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus
+            odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec
+            vehicula eget quam maximus interdum. Duis ultrices sapien eget
+            metus luctus finibus.
+        </p>
+        <a href="post.html" title="" class="read-more">
+            Read
+            <i class="fa fa-long-arrow-alt-right"></i>
+        </a>
+    </div>
+</div>
