b07182c2415149d2dbf36dd7ba36bc79e22682d1
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 22:56:49 +0700

    chore: tags component

---
+++ resources/views/components/blog/tags.blade.php
@@ -0,0 +1,23 @@
+<div class="widget widget-tags">
+    <h3 class="widget-title">Tags</h3>
+    <ul>
+        <li>
+            <a href="#" title="">Teachers</a>
+        </li>
+        <li>
+            <a href="#" title="">Lessons</a>
+        </li>
+        <li>
+            <a href="#" title="">Shelly</a>
+        </li>
+        <li>
+            <a href="#" title="">School</a>
+        </li>
+        <li>
+            <a href="#" title="">Theme</a>
+        </li>
+        <li>
+            <a href="#" title="">Class</a>
+        </li>
+    </ul>
+</div>
