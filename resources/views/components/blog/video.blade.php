26aa08af1cb04916326e3a5cbbd38d7724582a9b
NikarashiHatsu <yourlovelydev@gmaill.com>
2021-12-28 21:52:20 +0700

    chore: video component

---
+++ resources/views/components/blog/video.blade.php
@@ -0,0 +1,61 @@
+<div class="blog-post video-post">
+    <div class="blog-thumbnail">
+        <a href="#" title="">
+            <img
+                src="https://via.placeholder.com/1440x960"
+                alt=""
+                class="w-100"
+            />
+        </a>
+        <span class="category">
+            English, Teachers, Shelly
+        </span>
+        <a
+            href="https://www.youtube.com/watch?v=9MUDvEHkREM"
+            target="_blank"
+            title=""
+            class="video-play"
+        >
+            <img src="assets/img/play.png" alt="">
+        </a>
+    </div>
+    <div class="blog-info">
+        <ul class="meta">
+            <li>
+                <a href="#" title="">
+                    17/09/2020
+                </a>
+            </li>
+            <li>
+                <a href="#" title="">
+                    by Admin
+                </a>
+            </li>
+            <li>
+                <img src="assets/img/icon13.png" alt="" />
+                <a href="#" title="">
+                    Teachers,
+                </a>
+                <a href="#" title="">
+                    School
+                </a>
+            </li>
+        </ul>
+        <h3>
+            <a href="post.html" title="">
+                Video Post Format
+            </a>
+        </h3>
+        <p>
+            Nullam erat dolor, hendrerit id turpis laoreet, congue dapibus
+            odio. Duis tempor eros tortor, a ornare arcu egestas quis. Donec
+            vehicula eget quam maximus interdum. Duis ultrices sapien eget
+            metus luctus finibus. Morbi nec lobortis ante, sed condimentum
+            nisl. Nam laoreet ullamcorper magna et fringilla
+        </p>
+        <a href="post.html" title="" class="read-more">
+            Read
+            <i class="fa fa-long-arrow-alt-right"></i>
+        </a>
+    </div>
+</div>
