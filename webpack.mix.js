17ad8d238967c32cc99aaf43fa8d888fc628e2a3
NikarashiHatsu <yourlovelydev@gmail.com>
2022-02-04 03:37:24 +0700

    chore: production build

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix
     // Dashboard Assets
     .js('resources/js/app.js', 'public/js/app.js')
+    .js('resources/js/app-non-defer.js', 'public/js/app-non-defer.js')
     .postCss('resources/css/app.css', 'public/css/app.css', [
         require('postcss-import'),
         require('tailwindcss'),
